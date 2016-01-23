bd4361648f3d7c2225df570b6721fdc5caf0cef8
Laura Eck <laura.tatsutahime@gmail.com>
2016-01-23 14:54:58 +0900

    add migration for changes table

---
+++ db/migrate/change_tracker_migration.rb
@@ -0,0 +1,15 @@
+class ActsAsTaggableOnMigration < ActiveRecord::Migration
+  def self.up
+    create_table :changes do |t|
+      t.string :record_model
+      t.integer :record_id
+      t.integer :change_type, default: nil
+      t.text :changed_data
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :changes
+  end
+end
